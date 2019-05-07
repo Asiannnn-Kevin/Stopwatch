539e38d5b9ecb4ab4ea760229ebb7805df082dfb
Kevin <50320553+Asiannnn-Kevin@users.noreply.github.com>
2019-05-07 17:41:11 +0800

    Add files via upload

---
+++ Stopwatch/Stopwatch.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#define TRUE 1
+
+int main (void) {
+
+int hr = 0, min = 0, sec = 0;
+
+while (TRUE) {
+    printf("\nRemaining Time Left:\n\n");
+    printf("\t\t\t\t %3d hr : %3d min : %3d sec\r\n", hr, min, sec);
+
+    sec++;
+    sleep(1);
+
+    if (sec == 59) {
+        min++;
+        sec = 0;
+    }
+
+    if (min == 59) {
+        hr++;
+        min = 0;
+        sec = 0;
+    }
+    
+}
+
+return 0;
+
+
+}
